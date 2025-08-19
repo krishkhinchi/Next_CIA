3938ed3747e0caf779902a43d4d20ed591cb24af
Krish Khinchi <107798578+krishkhinchi@users.noreply.github.com>
2025-08-19 16:12:42 +0530

    test

--- Q1/q1/src/pages/blog/[id].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function BlogPostPage() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return <h1>Blog Post ID: {id}</h1>;
-}
